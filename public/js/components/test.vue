4a26058b1ef929fa861fb88471cdc030a05217ff
Alby Hernández <donfumero@gmail.com>
2020-04-19 03:29:27 +0100

    Code creation form is done. Just the logic is missing yet.

---
+++ public/js/components/test.vue
@@ -0,0 +1,12 @@
+<!-- my-component.vue -->
+<template>
+  <div>This will be pre-compiled</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- resources/views/modules/codes/creation.blade.php
+++ resources/views/modules/codes/creation.blade.php
@@ -16,14 +16,6 @@
                 <h3 class="mb-auto text-light">Creating a code</h3>
             </div>
         </div>
-        <div class="my-auto">
-            <!--
-            <button type="button" class="btn btn-primary shadow">
-                <i class="material-icons align-middle">add</i>
-                <span class="align-middle">New code</span>
-            </button>
-            -->
-        </div>
     </div>
     
     
@@ -34,38 +26,27 @@
             <input type="text" class="form-control" placeholder="Give it a name">
         </div>
         
-        <div class="form-group mb-5">
-            <button type="button" class="btn btn-sm btn-primary rounded-circle">
+        <div class="form-group mb-4">
+            <button type="button" class="btn btn-sm btn-secondary shadow" v-on:click="addTarget()">
                 <i class="material-icons align-middle">add</i>
             </button>
-            Agrega objetivos
+            <button type="button" class="btn btn-sm btn-secondary shadow" v-on:click="removeTarget()">
+                <i class="material-icons align-middle">remove</i>
+            </button>
+            <p class="text-muted small mt-2">Set your targets</p>
         </div>
         
-        <div class="row align-items-end m-0 border border-secondary rounded bg-secondary py-4 px-2">
-            <div class="col col-md-4">
-                <label class="small">Your target</label>
-                <select class="form-control" name="target[0][system]">
-                    <option value="android" selected>Android</option>
-                    <option value="ios">iOS</option>
-                    <option value="any">Any</option>
-                </select>
-            </div>
-            <div class="col col-md-6">
-                <label class="small">Destination url</label>
-                <input type="text" class="form-control" name="target[0][url]" placeholder="URL de destino">
-            </div>
-            <div class="col col-md-2">
-                <button type="button" class="btn btn-secondary active">
-                    <i class="material-icons align-middle">delete</i>
-                </button>
-            </div>
-        </div>
-        
-        <div class="form-group mt-5">
+        {{-- Targets --}}     
+        <codes-target-selector v-for="item in range" v-bind:name="item">
+        </codes-target-selector>
+   
+        {{-- Submit button --}}
+        <div class="form-group mt-5 ">
             <button type="submit" class="btn btn-primary">
                 Create!
             </button>
         </div>
+        
     </form>
     
 @endsection
@@ -75,7 +56,59 @@
 {{-- Javascript for this form --}}
 @push('scripts')
     <script>
-    
+        //Register Page header component
+        Vue.component('codes-target-selector', {
+            props: ['name'],
+            methods: {
+                setTargetName: function() {
+                    let name = this.$props.name;
+                    return "target["+name+"][system]";
+                },
+                setTargetUrl: function() {
+                    let name = this.$props.name;
+                    return "target["+name+"][url]";
+                }
+            },
+            template: `
+                <div class="my-4 shadow">
+                    <div class="row align-items-end m-0 border border-secondary rounded bg-secondary py-4 px-2">
+                        <div class="col col-md-4">
+                            <label class="small">Your target</label>
+                            <select class="form-control" :name="setTargetName()" >
+                                <option value="android" selected>Android</option>
+                                <option value="ios">iOS</option>
+                                <option value="any">Any</option>
+                            </select>
+                        </div>
+                        <div class="col">
+                            <label class="small">Destination url</label>
+                            <input type="text" class="form-control" :name="setTargetUrl()" placeholder="http://destination.url/...">
+                        </div>
+                    </div>
+                </div>
+            `
+        });
+        
+        
+        //Root Instance
+        var Vue = new Vue({
+            el: '#app',
+            data: {
+                range: 1
+            },
+
+            methods: {
+                addTarget: function() {
+                    this.range += 1;
+                },
+                removeTarget: function() {
+                    if ( this.range > 1 ) {
+                        this.range -= 1;
+                    }
+                }
+            }
+        })
+        
     </script>
 @endpush
 
