4d22b3284f29b2449c35b0f3cae401b56f2f2f61
Alby Hernández <donfumero@gmail.com>
2020-08-12 01:59:19 +0100

    New callback to perform a health check on internal.php file into RouteServiceProvider

--- routes/internal.php
+++ routes/internal.php
@@ -1,5 +1,8 @@
 <?php
 
+use Exception;
+use App\User;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,22 +17,36 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::get('/health', function(){
-    /*
-    return response()->json([
-        'status'    => 'error',
-        'message'   => 'route not found'
-    ], 404 );
-    */
+Route::get('/health', function() {
+    try{
+        # Make a request to the database
+        $collection = User::limit(1)->get();
+
+        # Check the result
+        if ( $collection->isEmpty() ) {
+            throw new Exception('DB query collection is empty');
+        }
+
+        # Return the response
+        return response()->json([
+            'status'    => 'success',
+            'message'   => 'query successful'
+        ], 200 );
+
+    } catch ( Exception $e ) {
+        Log::emergency($e);
+        return response()->json([
+            'status'    => 'error',
+            'message'   => 'query not successful'
+        ], 500 );
+    }
 });
     
 
 
 Route::any('/{any}', function () {
-    
     return response()->json([
         'status'    => 'error',
         'message'   => 'route not found'
     ], 404 );
-
 })->where('any', '.*');
