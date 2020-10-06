42438dca8a2542e6ad53741901bbb91f22911585
dalher <donfumero@gmail.com>
2020-10-06 02:36:25 +0100

    Fixed some classes on the AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,9 +6,8 @@ use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Facades\Validator;
-
-use \Illuminate\Support\Facades\App;
-use \Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
